8874e0bf9d2d188ba97fe2f3d491012b2ec2a14a
rwxrob <rob@rwx.gg>
2022-03-08 12:33:47 -0500

    Move Types into own package

---
+++ types/types.go
@@ -0,0 +1,144 @@
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+const UNKNOWN = 0
+
+// Types contains integer types with a lookup index (which must always
+// be kept in sync. The type 0 is always UNKNOWN. Structures that embed
+// Types should always require a slice of strings be passed to their
+// Init method which handles the creation of the Types.Map. Types should
+// usually not be changed once initialized.
+type Types struct {
+	Names Names `json:",omitempty"`
+	Map   Map   `json:",omitempty"`
+}
+
+// JSONL implements rwxrob/json.AsJSON.
+func (s *Types) JSON() ([]byte, error) { return json.Marshal(s) }
+
+// JSONL implements rwxrob/json.AsJSON.
+func (s *Types) JSONL() ([]byte, error) {
+	return json.MarshalIndent(s, "  ", "  ")
+}
+
+// String implements rwxrob/json.Stringer and fmt.Stringer.
+func (s Types) String() string {
+	byt, err := s.JSON()
+	if err != nil {
+		log.Print(err)
+	}
+	return string(byt)
+}
+
+// StringLong implements rwxrob/json.Stringer.
+func (s Types) StringLong() string {
+	byt, err := s.JSONL()
+	if err != nil {
+		log.Print(err)
+	}
+	return string(byt)
+}
+
+// String implements rwxrob/json.Printer.
+func (s *Types) Print() { fmt.Println(s.String()) }
+
+// PrintLong implements rwxrob/json.Printer.
+func (s *Types) PrintLong() { fmt.Println(s.StringLong()) }
+
+// Log implements rwxrob/json.Logger.
+func (s Types) Log() { log.Print(s.String()) }
+
+// LogLong implements rwxrob/json.Logger.
+func (s Types) LogLong() { log.Print(s.StringLong()) }
+
+// ---------------------------- Names ----------------------------
+
+// Names allows names to be looked up by integer value. O is always
+// UNKNOWN
+type Names []string
+
+// JSONL implements rwxrob/json.AsJSON.
+func (s *Names) JSON() ([]byte, error) { return json.Marshal(s) }
+
+// JSONL implements rwxrob/json.AsJSON.
+func (s *Names) JSONL() ([]byte, error) {
+	return json.MarshalIndent(s, "  ", "  ")
+}
+
+// String implements rwxrob/json.Stringer and fmt.Stringer.
+func (s Names) String() string {
+	byt, err := s.JSON()
+	if err != nil {
+		log.Print(err)
+	}
+	return string(byt)
+}
+
+// StringLong implements rwxrob/json.Stringer.
+func (s Names) StringLong() string {
+	byt, err := s.JSONL()
+	if err != nil {
+		log.Print(err)
+	}
+	return string(byt)
+}
+
+// String implements rwxrob/json.Printer.
+func (s *Names) Print() { fmt.Println(s.String()) }
+
+// PrintLong implements rwxrob/json.Printer.
+func (s *Names) PrintLong() { fmt.Println(s.StringLong()) }
+
+// Log implements rwxrob/json.Logger.
+func (s Names) Log() { log.Print(s.String()) }
+
+// LogLong implements rwxrob/json.Logger.
+func (s Names) LogLong() { log.Print(s.StringLong()) }
+
+// ----------------------------- Map -----------------------------
+
+// Map allows type integer values to be looked up by name.
+type Map map[string]int
+
+// JSONL implements rwxrob/json.AsJSON.
+func (s *Map) JSON() ([]byte, error) { return json.Marshal(s) }
+
+// JSONL implements rwxrob/json.AsJSON.
+func (s *Map) JSONL() ([]byte, error) {
+	return json.MarshalIndent(s, "  ", "  ")
+}
+
+// String implements rwxrob/json.Stringer and fmt.Stringer.
+func (s Map) String() string {
+	byt, err := s.JSON()
+	if err != nil {
+		log.Print(err)
+	}
+	return string(byt)
+}
+
+// StringLong implements rwxrob/json.Stringer.
+func (s Map) StringLong() string {
+	byt, err := s.JSONL()
+	if err != nil {
+		log.Print(err)
+	}
+	return string(byt)
+}
+
+// String implements rwxrob/json.Printer.
+func (s *Map) Print() { fmt.Println(s.String()) }
+
+// PrintLong implements rwxrob/json.Printer.
+func (s *Map) PrintLong() { fmt.Println(s.StringLong()) }
+
+// Log implements rwxrob/json.Logger.
+func (s Map) Log() { log.Print(s.String()) }
+
+// LogLong implements rwxrob/json.Logger.
+func (s Map) LogLong() { log.Print(s.StringLong()) }
