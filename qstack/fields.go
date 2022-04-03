8eb017ed6ffdaf43f9a6d5343ff0703e87063568
rwxrob <rob@rwx.gg>
2022-04-03 04:03:49 -0400

    Add qstack.Fields

---
+++ qstack/fields.go
@@ -0,0 +1,21 @@
+package qstack
+
+import "unicode"
+
+// Fields is exactly the same as strings.Fields (returning strings
+// separated by unicode.IsSpace) except it returns a qstack.QS instead.
+func Fields(in string) *QS[string] {
+	var field string
+	fields := New[string]()
+	for _, r := range []rune(in) {
+		if unicode.IsSpace(r) {
+			if len(field) > 0 {
+				fields.Push(field)
+				field = ""
+			}
+			continue
+		}
+		field += string(r)
+	}
+	return fields
+}
---
+++ qstack/fields_test.go
@@ -0,0 +1,35 @@
+package qstack_test
+
+import (
+	"fmt"
+
+	"github.com/rwxrob/structs/qstack"
+)
+
+func ExampleFields() {
+
+	text := `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+
+			Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+	`
+
+	fields := qstack.Fields(text)
+
+	fmt.Println(fields.Len)
+	fmt.Println(fields.Items()[0])
+	fmt.Println(fields.Items()[34])
+	fmt.Println(fields.Items()[68])
+
+	// Output:
+	// 69
+	// Lorem
+	// commodo
+	// laborum.
+}
